38c5e37fb0dfc88f4c47f5d0d754379e99c9505f
i066950 <wei.wu01@sap.com>
2020-03-10 17:53:03 +0800

    71: 分类、归档和标签页

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -5,7 +5,7 @@ import re
 from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Category, Tag
  
 def index(request):
     post_list = Post.objects.all()
@@ -23,4 +23,25 @@ def detail(request, pk):
     m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
     post.toc = m.group(1) if m is not None else '' 
     return render(request, 'blog/detail.html', context={'post': post})
+
+def archive(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,   #正常的py语法是create_time.year 这里是用做参数名，所以用双下划线替代点
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def tag(request, pk):
+    # 记得在开始部分导入 Category 类
+    tag = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=tag).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+
 
